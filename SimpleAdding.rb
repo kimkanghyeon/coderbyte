1be383b7dd697659420833147b60189ef671e9a2
김강현 <kimkang0907@gmail.com>
2015-07-29 01:22:46 +0900

    Create SimpleAdding.rb

---
+++ SimpleAdding.rb
@@ -0,0 +1,36 @@
+def SimpleAdding(num)
+
+  # code goes here
+  
+  
+  def a
+    (1..self).reduce(:+) || 1
+  end
+
+
+  return num.a
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleAdding(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
