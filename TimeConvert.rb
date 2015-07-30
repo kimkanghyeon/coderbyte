d8bc488c14a4cb14a700e8c48740f5ef0eb394ce
김강현 <kimkang0907@gmail.com>
2015-07-30 14:46:23 +0900

    Create TimeConvert.rb

---
+++ TimeConvert.rb
@@ -0,0 +1,31 @@
+def TimeConvert(num)
+
+  # code goes here
+    hour = num/60.round
+    minute = num % 60
+    return "#{hour}:#{minute}"
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+TimeConvert(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
